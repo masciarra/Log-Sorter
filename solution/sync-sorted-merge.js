5438570b09f8e292ebdac216251ab68552a734c0
masciarra <masciarra@usfca.edu>
2024-03-25 20:36:26 -0700

    modularized heap

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,47 +1,6 @@
 "use strict";
 
-function heapPush(heap, newKey) {
-  heap.push(newKey);
-
-  let curr = heap.length - 1;
-
-  while (curr > 0) {
-    let parent = Math.floor((curr - 1) / 2);
-    if (heap[curr].log.date < heap[parent].log.date) {
-      [heap[curr], heap[parent]] = [heap[parent], heap[curr]];
-      curr = parent;
-    } else {
-      break;
-    }
-  }
-}
-
-function heapPop(heap) {
-  const n = heap.length;
-  [heap[0], heap[n - 1]] = [heap[n - 1], heap[0]];
-
-  const removedKey = heap.pop();
-
-  let curr = 0;
-
-  while (2 * curr + 1 < heap.length) {
-    const leftIndex = 2 * curr + 1;
-    const rightIndex = 2 * curr + 2;
-    const minChildIndex =
-      rightIndex < heap.length &&
-      heap[rightIndex].log.date < heap[leftIndex].log.date
-        ? rightIndex
-        : leftIndex;
-    if (heap[minChildIndex].log.date < heap[curr].log.date) {
-      [heap[minChildIndex], heap[curr]] = [heap[curr], heap[minChildIndex]];
-      curr = minChildIndex;
-    } else {
-      break;
-    }
-  }
-
-  return removedKey;
-}
+const { heapPush, heapPop } = require("./heap.js");
 
 module.exports = (logSources, printer) => {
   const logHeap = [];
